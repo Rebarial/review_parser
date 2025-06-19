96b49b04e2387b8824e43c0560c33a93f6d91a50
Rebarial <reberalko@gmail.com>
2025-06-19 13:52:16 +1000

    try to fix selenium

--- review_parser/google_parser/tools/parser.py
+++ review_parser/google_parser/tools/parser.py
@@ -45,10 +45,13 @@ def google_date_parse(data):
 @logger.catch
 def parse(url:str, limit:Optional[int] = None) -> list[dict]:
     options = Options()
-    options.add_argument('--headless')  
-    options.add_argument('--no-sandbox')   
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920,1080')
+    options.add_argument('--remote-debugging-port=9222')
+    options.add_argument('--remote-debugging-address=0.0.0.0')
 
     driver = webdriver.Remote(
         command_executor='http://selenium:4444/wd/hub',
