128a41e9bdce41fcc6da3cbc189e1ccacb701e4f
Rebarial <reberalko@gmail.com>
2025-06-20 14:48:39 +1000

    celery result backend

--- review_parser/review_parser/settings.py
+++ review_parser/review_parser/settings.py
@@ -135,7 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CELERY_BROKER_URL = 'redis://redis:6379/0'
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_CACHE_BACKEND = 'django-cache'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
