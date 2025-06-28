e9c66f484d084e712c2842f8999ab2452f55aa02
Rebarial <reberalko@gmail.com>
2025-06-28 14:04:10 +1000

    add decimal places

--- review_parser/common_parser/serializers.py
+++ review_parser/common_parser/serializers.py
@@ -7,11 +7,20 @@ class OrganizationSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class BranchSerializer(serializers.ModelSerializer):
+
+    google_review_avg = serializers.DecimalField(max_digits=5, decimal_places=1, coerce_to_string=True)    
+    yandex_review_avg = serializers.DecimalField(max_digits=5, decimal_places=1, coerce_to_string=True)    
+    twogis_review_avg = serializers.DecimalField(max_digits=5, decimal_places=1, coerce_to_string=True)    
+    vlru_review_avg = serializers.DecimalField(max_digits=5, decimal_places=1, coerce_to_string=True)    
+
     class Meta:
         model = Branch
         fields = '__all__'
 
 class ReviewSerializer(serializers.ModelSerializer):
+
+    rating = serializers.DecimalField(max_digits=5, decimal_places=1, coerce_to_string=True)
+
     class Meta:
         model = Review
         fields = '__all__'
